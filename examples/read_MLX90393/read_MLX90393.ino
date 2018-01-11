dab152c861cf475b3621572ce39d3ea2b67fde99
Ted Yapo <ted.yapo@gmail.com>
2018-01-11 09:26:48 -0500

    modified example sketch to wait for character before sending data

--- examples/read_MLX90393/read_MLX90393.ino
+++ examples/read_MLX90393/read_MLX90393.ino
@@ -8,19 +8,22 @@ MLX90393 mlx;
 
 void setup(){
   // DRDY line connected to A3 (omit third parameter to used timed reads)
-  uint8_t status = mlx.begin(0, 0, A3);
+  uint8_t status = mlx.begin(0, 0);
   Serial.begin(9600);
 }
 
 void loop(){
   MLX90393::txyz data;
-  mlx.readData(data);
-  Serial.print(data.x);
-  Serial.print(" ");
-  Serial.print(data.y);
-  Serial.print(" ");
-  Serial.print(data.z);
-  Serial.print(" ");
-  Serial.println(data.t);
-  delay(1);
+  if (Serial.available()){
+    Serial.read();
+    mlx.readData(data);
+    Serial.print(data.x);
+    Serial.print(" ");
+    Serial.print(data.y);
+    Serial.print(" ");
+    Serial.print(data.z);
+    Serial.print(" ");
+    Serial.println(data.t);
+  //delay(1);
+  }
 }
