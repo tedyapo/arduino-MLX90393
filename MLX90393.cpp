4deb00407dcaffc85a78be0618dac30720598e0f
udoklein <git@blinkenlight.net>
2018-01-17 19:27:58 +0100

    changed indentation for the RETURN STATUS_ERROR code
    I found the return STATUS_ERROR somewhat obstructs the view
    on what is really going on. Thus I tried to make the actual
    code a little bit more prominent. The price to pay is to
    somewhat deviate from the overall indentation style.
    
    However this is a matter of personal taste. If you like it
    merge it, otherwise reject it.

--- MLX90393.cpp
+++ MLX90393.cpp
@@ -97,15 +97,10 @@ MLX90393::
 sendCommand(uint8_t cmd)
 {
   _i2cPort->beginTransmission(I2C_address);
-  if (_i2cPort->write(cmd) != 1){
-    return STATUS_ERROR;
-  }
-  if (_i2cPort->endTransmission()){
-    return STATUS_ERROR;
-  }
-  if (_i2cPort->requestFrom(I2C_address, uint8_t(1)) != 1){
-    return STATUS_ERROR;
-  }
+  if (_i2cPort->write(cmd) != 1){ return STATUS_ERROR; }
+  if (_i2cPort->endTransmission()){ return STATUS_ERROR; }
+  if (_i2cPort->requestFrom(I2C_address, uint8_t(1)) != 1){ return STATUS_ERROR; }
+
   return _i2cPort->read();
 }
 
@@ -211,37 +206,24 @@ MLX90393::
 readRegister(uint8_t address, uint16_t& data)
 {
   _i2cPort->beginTransmission(I2C_address);
+  if (_i2cPort->write(CMD_READ_REGISTER) != 1){ return STATUS_ERROR; }
+  if (_i2cPort->write((address & 0x3f)<<2) != 1){ return STATUS_ERROR; }
+  if (_i2cPort->endTransmission()){ return STATUS_ERROR; }
+  if (_i2cPort->requestFrom(I2C_address, uint8_t(3)) != 3){ return STATUS_ERROR; }
 
-  if (_i2cPort->write(CMD_READ_REGISTER) != 1){
-    return STATUS_ERROR;
-  }
-  if (_i2cPort->write((address & 0x3f)<<2) != 1){
-    return STATUS_ERROR;
-  }
-  if(_i2cPort->endTransmission()){
-    return STATUS_ERROR;
-  }
-  if (_i2cPort->requestFrom(I2C_address, uint8_t(3)) != 3){
-    return STATUS_ERROR;
-  }
-  uint8_t status, b_h, b_l;
-  if (_i2cPort->available()){
-    status = _i2cPort->read();
-  } else {
-    return STATUS_ERROR;
-  }
-  if (_i2cPort->available()){
-    b_h = _i2cPort->read();
-  } else {
-    return STATUS_ERROR;
-  }
-  if (_i2cPort->available()){
-    b_l = _i2cPort->read();
-  } else {
-    return STATUS_ERROR;
-  }
+  uint8_t status;
+  if (!_i2cPort->available()){ return STATUS_ERROR; }
+  status = _i2cPort->read();
+
+  uint8_t b_h;
+  if (!_i2cPort->available()){ return STATUS_ERROR; }
+  b_h = _i2cPort->read();
+
+  uint8_t b_l;
+  if (!_i2cPort->available()){ return STATUS_ERROR; }
+  b_l = _i2cPort->read();
+
   data = (uint16_t(b_h)<<8) | b_l;
-
   cache_set(address, data);
   return status;
 }
@@ -255,29 +237,15 @@ writeRegister(uint8_t address, uint16_t data)
   cache_invalidate();
 
   _i2cPort->beginTransmission(I2C_address);
-  if ( _i2cPort->write(CMD_WRITE_REGISTER) != 1){
-    return STATUS_ERROR;
-  }
-  if (_i2cPort->write((data & 0xff00) >> 8) != 1){
-    return STATUS_ERROR;
-  }
-  if (_i2cPort->write(data & 0x00ff) != 1){
-    return STATUS_ERROR;
-  }
-  if (_i2cPort->write((address & 0x3f)<<2) != 1){
-    return STATUS_ERROR;
-  }
-  if (_i2cPort->endTransmission()){
-    return STATUS_ERROR;
-  }
-  if (_i2cPort->requestFrom(I2C_address, uint8_t(1)) != 1){
-    return STATUS_ERROR;
-  }
-  if (_i2cPort->available()){
-    return _i2cPort->read();
-  } else {
-    return STATUS_ERROR;
-  }
+  if (_i2cPort->write(CMD_WRITE_REGISTER) != 1){ return STATUS_ERROR; }
+  if (_i2cPort->write((data & 0xff00) >> 8) != 1){ return STATUS_ERROR; }
+  if (_i2cPort->write(data & 0x00ff) != 1){ return STATUS_ERROR; }
+  if (_i2cPort->write((address & 0x3f)<<2) != 1){ return STATUS_ERROR; }
+  if (_i2cPort->endTransmission()){ return STATUS_ERROR; }
+  if (_i2cPort->requestFrom(I2C_address, uint8_t(1)) != 1){ return STATUS_ERROR; }
+  if (!_i2cPort->available()){ return STATUS_ERROR; }
+
+  return _i2cPort->read();
 }
 
 uint8_t
